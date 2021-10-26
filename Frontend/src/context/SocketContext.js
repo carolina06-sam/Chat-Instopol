ca35704c81d51441a4f2e409ef46f23d9544e94a
MALEROCHA03 <alejalafafita@gmail.com>
2021-10-26 17:26:29 -0500

    despliegue

--- Frontend/src/context/SocketContext.js
+++ Frontend/src/context/SocketContext.js
@@ -12,7 +12,7 @@ export const SocketContext = createContext();
 
 export const SocketProvider = ({ children }) => {
     const { socket, online, conectarSocket, desconectarSocket } = useSocket(
-        "http://localhost:8080"
+        "https://backchat1.herokuapp.com"
     );
     const { auth } = useContext(AuthContext);
     const { dispatch } = useContext(ChatContext);
